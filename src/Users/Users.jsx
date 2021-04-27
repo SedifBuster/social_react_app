dc15c8d62043bce391139279ed8d98b5c6878d96
Artem <68074370+SedifBuster@users.noreply.github.com>
2021-04-27 23:56:40 +0300

    fix

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,31 +1,41 @@
 import React from "react";
-import style from './Users.module.css'
-import * as axios from 'axios'
-import userPhoto from '../assets/images/user.jpg'
+import style from "./Users.module.css";
+import userPhoto from "../assets/images/user.jpg";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items)
-        })
+function Users(props) {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+    let pages = []
+    for (let i=1; i <= pagesCount; i++) {
+        pages.push(i)
     }
+    return <div>
+        <div>
+            {pages.map(page => {
+                return <span className={props.currentPage === page && style.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(page)
+                             }}>{page}</span>
+            })}
+        </div>
 
-    render() {
-        return (
-            <div>
-
-                {
-                    this.props.users.map(users => <div key={users.id}>
+        {
+            props.users.map(users => <div key={users.id}>
                     <span>
                         <div>
-                            <img src={users.photos.small != null ? users.photos.small : userPhoto} className={style.photo}/>
+                            <img src={users.photos.small != null ? users.photos.small : userPhoto}
+                                 className={style.photo}/>
                         </div>
                         <div>
-                            { users.followed ? <button onClick={() => {this.props.unfollow(users.id)}}>Unfollow</button>
-                                : <button onClick={() => {this.props.follow(users.id)}}>Follow</button>}
+                            {users.followed ? <button onClick={() => {
+                                    props.unfollow(users.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(users.id)
+                                }}>Follow</button>}
                         </div>
                     </span>
-                        <span>
+                <span>
                         <span>
                             <div>
                                 {users.name}
@@ -43,20 +53,10 @@ class Users extends React.Component {
                             </div>
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        )
-    }
-
+            </div>)
+        }
+    </div>
 }
-
-
-
-
-
-
-
 
 
 export default Users
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,10 +1,15 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 100,
+    currentPage: 1
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -31,7 +36,11 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users}
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage}
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.count}
         default:
             return state
     }
@@ -54,6 +63,20 @@ export let setUsersAC = (users) => {
     return {
         type: SET_USERS,
         users
+    }
+
+}
+export let setCurrentPageAC = (currentPage) => {
+    return {
+        type: SET_CURRENT_PAGE,
+        currentPage: currentPage
+    }
+}
+
+export let setUsersTotalCountAC = (totalUsersCount) => {
+    return {
+        type: SET_TOTAL_USERS_COUNT,
+        count: totalUsersCount
     }
 }
 
